39053a0a42166e111ada7e08e5542ec5f03e6ef3
ovidiubute <ovidiu.bute@gmail.com>
2017-04-16 16:32:40 +0300

    Change message. #1

--- src/client/main-chat/MainChat.js
+++ src/client/main-chat/MainChat.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class MainChat extends React.Component {
   render() {
-    return <p>Hello from the backend!</p>;
+    return <p>Hello from the backend, this is a new PR!</p>;
   }
 }
 
