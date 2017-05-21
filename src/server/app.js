4ab5f85bf05696633369891c2adf137560370390
ovidiubute <ovidiu.bute@gmail.com>
2017-05-21 14:17:23 +0300

    Pivot to MongoDB

--- src/server/app.js
+++ src/server/app.js
@@ -8,97 +8,89 @@ const path = require("path");
 const React = require("react");
 const { renderToString } = require("react-dom/server");
 const { createClient } = require("redis");
-const nano = require("nano")("http://couchdb:5984");
+const MongoClient = require('mongodb').MongoClient
 const MainChat = require("../client/main-chat").default;
 
-/**
- * Get last 10 messages from the database
- * to serve as starting point for SSR of our app
- */
-async function getLastMessages(db) {
-  return new Promise((resolve, reject) =>
-    db.list({ limit: 10, include_docs: true }, (err, body) => {
-      if (err) {
-        reject(err);
-      }
+MongoClient.connect("mongodb://mongodb/isomorphic-chat", (err, db) => {
+  if (err) {
+    throw new Error("Unable to connect to MongoDB. Aborting!");
+  }
 
-      resolve(body.rows);
-    })
-  );
-}
+  /**
+   * Get last 10 messages from the database
+   * to serve as starting point for SSR of our app
+   */
+  async function getLastMessages() {
+    return db.collection("messages").find({}).limit(10).toArray();
+  }
 
-// Init Redis connection
-const pub = createClient({
-  host: "redis"
-});
-const sub = createClient({
-  host: "redis"
-});
-
-// Subscribe to receive all messages on channel
-sub.subscribe("main-channel");
-
-// Init backend app
-const app = new Koa();
-
-// DB
-const messagesDB = nano.use("ic-messages");
-
-// Init socket.io chat endpoint
-const io = new IO();
-
-// Attach socket.io handler to Koa app
-io.attach(app);
-
-sub.on("message", (channel, message) => {
-  // Broadcasts to all connections
-  io.broadcast("message", message);
-});
-
-// On incoming socket message, publish to Redis and save to DB
-io.on("message", ctx => {
-  const timestamp = Date.now();
-
-  pub.publish("main-channel", JSON.stringify(ctx.data));
-  messagesDB.insert({
-    _id: `${ctx.data.userId}-${timestamp}`,
-    msg: ctx.data.msg,
-    timestamp
+  // Init Redis connection
+  const pub = createClient({
+    host: "redis"
   });
-});
-
-// Attach render middleware
-render(app, {
-  root: path.join(__dirname, "view"),
-  layout: "template",
-  viewExt: "ejs",
-  cache: true
-});
-
-// Koa static handler
-app.use(serve(path.resolve(__dirname, "..", "client", "dist")));
-
-// Koa main handler
-app.use(async ctx => {
-  const messages = await getLastMessages(messagesDB);
-
-  await ctx.render("main", {
-    reactOutput: renderToString(
-      React.createElement(MainChat, {
-        messages
-      })
-    )
+  const sub = createClient({
+    host: "redis"
   });
+
+  // Subscribe to receive all messages on channel
+  sub.subscribe("main-channel");
+
+  // Init backend app
+  const app = new Koa();
+
+  // Init socket.io chat endpoint
+  const io = new IO();
+
+  // Attach socket.io handler to Koa app
+  io.attach(app);
+
+  sub.on("message", (channel, message) => {
+    // Broadcasts to all connections
+    io.broadcast("message", message);
+  });
+
+  // On incoming socket message, publish to Redis and save to DB
+  io.on("message", ctx => {
+    pub.publish("main-channel", JSON.stringify(ctx.data));
+    db.collection("messages").insert({
+      msg: ctx.data.msg,
+      timestamp: Date.now()
+    });
+  });
+
+  // Attach render middleware
+  render(app, {
+    root: path.join(__dirname, "view"),
+    layout: "template",
+    viewExt: "ejs",
+    cache: true
+  });
+
+  // Koa static handler
+  app.use(serve(path.resolve(__dirname, "..", "client", "dist")));
+
+  // Koa main handler
+  app.use(async ctx => {
+    const messages = await getLastMessages();
+
+    console.log(messages);
+
+    await ctx.render("main", {
+      reactOutput: renderToString(
+        React.createElement(MainChat, {
+          messages
+        })
+      )
+    });
+  });
+
+  // Main entry point
+  if (process.env.NODE_ENV === "test") {
+    // ES6 exports not allowed in a dynamic context
+    module.exports = app.callback();
+  } else {
+    app.listen(7001);
+  }
+
+  app.on("error", err2 => console.error(err2.stack));
 });
-
-// Main entry point
-if (process.env.NODE_ENV === "test") {
-  // ES6 exports not allowed in a dynamic context
-  module.exports = app.callback();
-} else {
-  app.listen(7001);
-}
-
-// Error handler
-// eslint-disable-next-line no-console
-app.on("error", err => console.error(err.stack));
--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,5 @@
 require("regenerator-runtime/runtime");
 require("babel-register");
 
-const nano = require("nano")("http://couchdb:5984");
-
-/**
- * Ensure DB is created
- * @param {String} name DB name
- */
-async function ensureDatabase(name) {
-  await new Promise((resolve, reject) =>
-    nano.db.list((err, body) => {
-      if (err) {
-        return reject(err);
-      }
-
-      if (body.includes(name)) {
-        return resolve(nano.db.use(name));
-      }
-
-      return nano.db.create(name, err2 => {
-        if (!err2) {
-          return resolve(nano.db.use(name));
-        }
-
-        return reject(err2);
-      });
-    })
-  );
-}
-
 // Start backend
-ensureDatabase("ic-messages").then(() => require("./app"));
+require("./app");
